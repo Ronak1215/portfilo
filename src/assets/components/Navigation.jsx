13b57b9748e5c0f9052edcedbd40d71a4c05a5c5
Ronak1215 <ronakchitalia28@gmail.com>
2024-07-01 17:14:09 +0530

    behance link update

--- src/assets/components/Navigation.jsx
+++ src/assets/components/Navigation.jsx
@@ -16,7 +16,7 @@ function Navigation() {
           </Link>
           <div className="nav-right">
             <a href="https://www.linkedin.com/in/brijesh-joshi-a7b64126/" target='_blank'><i className="ri-linkedin-fill"></i></a>
-            <a href="https://www.linkedin.com/in/brijesh-joshi-a7b64126/" target='_blank'><i className="ri-behance-fill"></i></a>
+            <a href="https://www.behance.net/brixcreative" target='_blank'><i className="ri-behance-fill"></i></a>
           </div>
         </nav>
     </div>
